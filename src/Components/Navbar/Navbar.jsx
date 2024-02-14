be53d6947eed56da13c50bf2bcccb9845e728d4a
Sergey87 <serge.techno@gmail.com>
2024-02-14 23:52:26 +0300

    start project

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import './Navbar.css';
+import menu_icon from '../../assets/menu.png';
+import logo from '../../assets/logo.png';
+import search_icon from '../../assets/search.png';
+import upload_icon from '../../assets/upload.png';
+import more_icon from '../../assets/more.png';
+import notification_icon from '../../assets/notification.png';
+import profile_icon from '../../assets/jack.png';
+
+const Navbar = () => {
+  return (
+    <nav className='flex-div'>
+      <div className='nav-left flex-div'>
+        <img className='menu-icon' src={menu_icon} alt='menu pic' />
+        <img className='logo' src={logo} alt='logo pic' />
+      </div>
+
+      <div className='nav-middle flex-div'>
+        <input type='text' placeholder='Search' />
+        <img src={search_icon} alt='search pic' />
+      </div>
+
+      <div className='nav-right flex-div'>
+        <img src={upload_icon} alt='upload pic' />
+        <img src={more_icon} alt='more pic' />
+        <img src={notification_icon} alt='note pic' />
+        <img className='user0icon' src={profile_icon} alt='profile pic' />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

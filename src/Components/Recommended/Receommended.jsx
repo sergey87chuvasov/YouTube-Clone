9103f15cac42ca76ccec982abdab0e12037191b8
Sergey87 <serge.techno@gmail.com>
2024-02-22 00:04:05 +0300

    finished recommended component

--- src/Components/Recommended/Receommended.jsx
+++ src/Components/Recommended/Receommended.jsx
@@ -1,38 +1,47 @@
 import './Recommended.css';
 
-import thumbnail1 from '../../assets/thumbnail1.png';
-import thumbnail2 from '../../assets/thumbnail2.png';
-import thumbnail3 from '../../assets/thumbnail3.png';
-import thumbnail4 from '../../assets/thumbnail4.png';
-import thumbnail5 from '../../assets/thumbnail5.png';
-import thumbnail6 from '../../assets/thumbnail6.png';
-import thumbnail7 from '../../assets/thumbnail7.png';
-import thumbnail8 from '../../assets/thumbnail8.png';
-import { useState } from 'react';
+// import thumbnail1 from '../../assets/thumbnail1.png';
+// import thumbnail2 from '../../assets/thumbnail2.png';
+// import thumbnail3 from '../../assets/thumbnail3.png';
+// import thumbnail4 from '../../assets/thumbnail4.png';
+// import thumbnail5 from '../../assets/thumbnail5.png';
+// import thumbnail6 from '../../assets/thumbnail6.png';
+// import thumbnail7 from '../../assets/thumbnail7.png';
+// import thumbnail8 from '../../assets/thumbnail8.png';
 
+import { useEffect, useState } from 'react';
 import { API_KEY, value_converter } from '../../data';
 
 const Recommended = ({ categoryId }) => {
   const [apiData, setApiData] = useState([]);
 
   const fetchData = async () => {
-    const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
+    const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=40&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
     await fetch(relatedVideo_url)
       .then((res) => res.json())
       .then((data) => setApiData(data.items));
   };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className='recommended'>
-      <div className='side-video-list'>
-        <img src={thumbnail1} alt='thumb pic' />
-        <div className='vid-info'>
-          <h4>Best chanel that help you to be a web developer</h4>
-          <p>GreatStack</p>
-          <p>194K Views</p>
-        </div>
-      </div>
-      <div className='side-video-list'>
+      {apiData.map((item, index) => {
+        return (
+          <div key={index} className='side-video-list'>
+            <img src={item.snippet.thumbnails.medium.url} alt='thumb pic' />
+            <div className='vid-info'>
+              <h4>{item.snippet.title}</h4>
+              <p>{item.snippet.channelTitle}</p>
+              <p>{value_converter(item.statistics.viewCount)} Views</p>
+            </div>
+          </div>
+        );
+      })}
+
+      {/* <div className='side-video-list'>
         <img src={thumbnail2} alt='thumb pic' />
         <div className='vid-info'>
           <h4>Best chanel that help you to be a web developer</h4>
@@ -87,7 +96,7 @@ const Recommended = ({ categoryId }) => {
           <p>GreatStack</p>
           <p>194K Views</p>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
